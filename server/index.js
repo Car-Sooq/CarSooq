1749000e1bf1233da5e27b59368d59a81e96fc5c
rahaf83 <rahaf.migdad@hotmail.com>
2020-12-08 22:47:57 +0200

    test

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const path = require('path');
 const bcrypt = require('bcrypt');
+
 app.use(express.json());
 app.use(cors());
 app.use(express.static(__dirname + '/../react-client/dist'));
